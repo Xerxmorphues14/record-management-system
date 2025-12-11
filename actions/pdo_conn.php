c20620fd9f416639f7832ef181dc71bded3d39f2
Christian Ferrer <christianferrer765@gmail.com>
2025-12-11 19:32:04 +0800

    add connection to the session of database and server

---
+++ actions/pdo_conn.php
@@ -0,0 +1,24 @@
+<?php 
+$server ="localhost";
+$username = "root";
+$password = "";
+$database = "";
+
+
+session_start();
+
+try {
+    $pdo = new PDO("mysql:host=$server;dbname=$db_name", $username, $password);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+} catch (PDOException $e) {
+    $error =  "Connection failed: " . $e->getMessage();
+    $_SESSION['error'] = $error;
+    echo $error;
+}
+
+
+
+
+?>
