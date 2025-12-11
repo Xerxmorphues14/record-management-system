33fc5bb546e03d11975d2498f39e677a68cd8765
Christian Ferrer <christianferrer765@gmail.com>
2025-12-11 23:37:37 +0800

    change of css and index.php to student and dean label

--- index.php
+++ index.php
@@ -20,16 +20,14 @@ include "actions/pdo_conn.php";
 </head>
 
 <body>
-<a href="https://front.codes/" class="logo" target="_blank">
-		<img src="https://assets.codepen.io/1462889/fcy.png" alt="">
-	</a>
+
 
 	<div class="section">
 		<div class="container">
 			<div class="row full-height justify-content-center">
 				<div class="col-12 text-center align-self-center py-5">
 					<div class="section pb-5 pt-5 pt-sm-2 text-center">
-						<h6 class="mb-0 pb-3"><span>USER</span><span>ADMIN</span></h6>
+						<h6 class="mb-0 pb-3"><span>STUDENT</span><span>DEAN</span></h6>
 			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
 			          	<label for="reg-log"></label>
 						<div class="card-3d-wrap mx-auto">
@@ -37,7 +35,7 @@ include "actions/pdo_conn.php";
 								<div class="card-front">
 									<div class="center-wrap">
 										<div class="section text-center">
-											<h4 class="mb-4 pb-3">Log In</h4>
+											<h4 class="mb-4 pb-3">STUDENT</h4>
 											<div class="form-group">
 												<input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off">
 												<i class="input-icon uil uil-at"></i>
@@ -53,7 +51,7 @@ include "actions/pdo_conn.php";
 								<div class="card-back">
 									<div class="center-wrap">
 										<div class="section text-center">
-											<h4 class="mb-4 pb-3">Sign Up</h4>
+											<h4 class="mb-4 pb-3">DEAN</h4>
 											<div class="form-group">
 												<input type="text" name="logname" class="form-style" placeholder="Your Full Name" id="logname" autocomplete="off">
 												<i class="input-icon uil uil-user"></i>
