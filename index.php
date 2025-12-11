9d3d3e3d29ffabcb096f354714ff9fa165e37ce7
Christian Ferrer <christianferrer765@gmail.com>
2025-12-11 22:58:25 +0800

    add sessions and connection of database and server

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php 
+session_start();
 
+include "actions/pdo_conn.php";
+
+
+
+?>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
